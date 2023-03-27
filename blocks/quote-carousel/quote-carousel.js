ba739463ff72f6afb46d8c3c768fd1d2a6c9eeb1
Iryna Lagno <ilagno@adobe.com>
2023-03-27 13:31:33 -0500

    - add quote carousel block

--- blocks/quote-carousel/quote-carousel.js
+++ blocks/quote-carousel/quote-carousel.js
@@ -93,10 +93,12 @@ function startAutoScroll(block, interval = DEFAULT_SCROLL_INTERVAL_MS) {
  */
 async function getContent(block) {
   const url = block.querySelector('div > div > div:nth-child(2) > a').href;
-  let data = { data: {} };
+  let data = { data: [] };
   try {
     const resp = await fetch(url);
-    data = await resp.json();
+    if (resp.ok) {
+      data = resp.json();
+    }
   } catch (error) {
     // eslint-disable-next-line no-console
     console.error('Failed to load block content', error);
@@ -107,56 +109,58 @@ async function getContent(block) {
 export default async function decorate(block) {
   const blockId = crypto.randomUUID();
   const content = await getContent(block);
-  block.querySelector('div:nth-child(1)');
   // generate carousel content from loaded data
   const slidesContainer = document.createElement('div');
   slidesContainer.classList.add('carousel-content');
   block.setAttribute('id', blockId);
   numChildren[blockId] = content.total;
-  [...content.data].forEach((row) => {
-    const rowContent = document.createElement('div');
-    rowContent.classList.add('item');
-    rowContent.innerHTML = `
+  block.innerHTML = '';
+  if (content.data.length > 0) {
+    [...content.data].forEach((row) => {
+      const rowContent = document.createElement('div');
+      rowContent.classList.add('item');
+      rowContent.innerHTML = `
                 <p class="quote">"${row.quote}"</p>
                 <p class="author">${row.author}</p>
                 <p class="position">${row.position}</p>
                 `;
-    rowContent.classList.add('item');
-    slidesContainer.appendChild(rowContent);
-  });
-  slidesContainer.children[0].setAttribute('active', true);
+      rowContent.classList.add('item');
+      slidesContainer.appendChild(rowContent);
+    });
+    slidesContainer.children[0].setAttribute('active', true);
 
-  // generate container for carousel controls
-  const controlsContainer = document.createElement('div');
-  controlsContainer.classList.add('controls-container');
-  controlsContainer.innerHTML = `
+    // generate container for carousel controls
+    const controlsContainer = document.createElement('div');
+    controlsContainer.classList.add('controls-container');
+    controlsContainer.innerHTML = `
     <div class="pagination">
         <span class="index">1</span>
         &nbsp;of&nbsp;
         <span class="total">${numChildren[blockId]}</span>
     </div>
-    <button name="prev" aria-label="Previous" class="control-button" disabled><svg><use xlink:href="../../icons/arrow.svg#carrot"/></svg></button>
+    <button name="prev" aria-label="Previous" class="control-button" disabled><svg><use xlink:href="/icons/icons.svg#carrot"/></svg></button>
     <button name="next" aria-label="Next" class="control-button"><svg><use xlink:href="/icons/icons.svg#carrot"/></svg></button>
   `;
-  block.replaceChildren(slidesContainer, controlsContainer);
+    block.replaceChildren(slidesContainer, controlsContainer);
 
-  const nextButton = block.querySelector('button[name="next"]');
-  const prevButton = block.querySelector('button[name="prev"]');
-  const indexElement = block.querySelector('.index');
-  nextButton.addEventListener('click', () => {
-    const currentIndex = getCurrentSlideIndex(slidesContainer);
+    const nextButton = block.querySelector('button[name="next"]');
+    const prevButton = block.querySelector('button[name="prev"]');
+    const indexElement = block.querySelector('.index');
+    nextButton.addEventListener('click', () => {
+      const currentIndex = getCurrentSlideIndex(slidesContainer);
 
-    switchSlide((currentIndex + 1) % numChildren[blockId], block);
-  });
-  prevButton.addEventListener('click', () => {
-    const currentIndex = getCurrentSlideIndex(slidesContainer);
-    switchSlide(
-      (((currentIndex - 1) % numChildren[blockId]) + numChildren[blockId]) % numChildren[blockId],
-      block,
-    );
-  });
-  indexElement.addEventListener('startAutoScroll', () => {
-    startAutoScroll(block);
-  });
-  indexElement.dispatchEvent(event);
+      switchSlide((currentIndex + 1) % numChildren[blockId], block);
+    });
+    prevButton.addEventListener('click', () => {
+      const currentIndex = getCurrentSlideIndex(slidesContainer);
+      switchSlide(
+        (((currentIndex - 1) % numChildren[blockId]) + numChildren[blockId]) % numChildren[blockId],
+        block,
+      );
+    });
+    indexElement.addEventListener('startAutoScroll', () => {
+      startAutoScroll(block);
+    });
+    indexElement.dispatchEvent(event);
+  }
 }
