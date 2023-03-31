3c8ccb20c39e6b4163ea8d9ef4db0fcaa53dc11c
Bryan Stopp <bryan.stopp@gmail.com>
2023-03-31 11:29:22 -0500

    use hover not click for opening nav

--- blocks/toc/toc.js
+++ blocks/toc/toc.js
@@ -8,26 +8,11 @@ const isDesktop = window.matchMedia('(min-width: 600px)');
 export default async function decorate(block) {
   const nav = document.createElement('nav');
   nav.setAttribute('aria-expanded', isDesktop.matches);
-
-  const opener = document.createElement('a');
-  opener.href = '#';
-  opener.textContent = 'Jump To';
-
-  nav.append(opener);
+  nav.append(block.querySelector('p'));
   nav.append(block.querySelector('ul'));
   block.replaceChildren(nav);
 
   isDesktop.addEventListener('change', () => {
     nav.setAttribute('aria-expanded', isDesktop.matches);
-  });
-
-  opener.addEventListener('click', (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    if (nav.getAttribute('aria-expanded') === 'true') {
-      nav.setAttribute('aria-expanded', 'false');
-    } else {
-      nav.setAttribute('aria-expanded', 'true');
-    }
   });
 }
