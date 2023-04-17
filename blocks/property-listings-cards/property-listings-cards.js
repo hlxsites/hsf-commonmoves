7248aa86da223899cdaebcba6b9e3e7b374b6ee7
Amy Zhao <azhao@adobe.net>
2023-04-17 15:52:39 -0500

    implement block config

--- blocks/property-listings-cards/property-listings-cards.js
+++ blocks/property-listings-cards/property-listings-cards.js
@@ -1,6 +1,17 @@
-import { decorateIcons } from '../../scripts/lib-franklin.js';
+import { decorateIcons, readBlockConfig } from '../../scripts/lib-franklin.js';
 
-const COMMONMOVES_API_PROPERTY = 'https://www.bhhs.com/bin/bhhs/CregPropertySearchServlet?SearchType=Map&MinPrice=800000&PropertyType=1,2,4&ApplicationType=FOR_SALE&ListingStatus=1&NewListing=true&Sort=DATE_DESCENDING&PageSize=10&Page=1&NorthEastLatitude=42.88103227227745&NorthEastLongitude=-68.5569598543709&SouthWestLatitude=40.731686815498&SouthWestLongitude=-71.8089129793709&SearchParameter=%7B%22type%22:%22FeatureCollection%22,%22features%22:%5B%7B%22type%22:%22Feature%22,%22properties%22:%7B%7D,%22geometry%22:%7B%22type%22:%22Polygon%22,%22coordinates%22:%5B%5B%5B-71.8089129793709,42.88103227227745%5D,%5B-71.8089129793709,40.731686815498%5D,%5B-68.5569598543709,40.731686815498%5D,%5B-68.5569598543709,42.88103227227745%5D,%5B-71.8089129793709,42.88103227227745%5D%5D%5D%7D%7D%5D%7D&MapSearchType=MapBounds&listingAgentId=&ucsid=false';
+const COMMONMOVES_API_BASE = 'https://www.bhhs.com';
+
+const defaultParameters = {
+  minprice: '800000',
+  newlisting: 'true',
+  sort: 'DATE_DESCENDING',
+  pagesize: '8',
+  maximumlatitude: '42.88103227227745',
+  maximumlongitude: '-68.5569598543709',
+  minimumlatitude: '40.731686815498',
+  minimumlongitude: '-71.8089129793709',
+};
 
 function createCard(listing) {
   let html = `
@@ -95,13 +106,25 @@ function createCard(listing) {
 }
 
 export default async function decorate(block) {
-  const resp = await fetch(COMMONMOVES_API_PROPERTY);
+  const config = readBlockConfig(block);
+  const apiParameters = {
+    minprice: config.minprice || defaultParameters.minprice,
+    newlisting: config.newlisting || defaultParameters.newlisting,
+    pagesize: config.pagesize || defaultParameters.pagesize,
+    maximumlatitude: config['max-lat'] || config['maximum-latitude'] || config['maximum-lat'] || config['max-latitude'] || defaultParameters.maximumlatitude,
+    maximumlongitude: config['max-lon'] || config['maximum-longitude'] || config['maximum-lon'] || config['max-longitude'] || defaultParameters.maximumlongitude,
+    minimumlatitude: config['min-lat'] || config['minimum-latitude'] || config['minimum-lat'] || config['min-latitude'] || defaultParameters.minimumlatitude,
+    minimumlongitude: config['min-lon'] || config['minimum-longitude'] || config['minimum-lon'] || config['min-longitude'] || defaultParameters.minimumlongitude,
+  };
+  apiParameters.sort = config['sort-by'] && config['sort-direction'] ? `${config['sort-by']}_${config['sort-direction']}`.toUpperCase() : defaultParameters.sort;
+  apiParameters.searchparameter = encodeURIComponent(`{"type":"FeatureCollection","features":[{"type":"Feature","properties":{},"geometry":{"type":"Polygon","coordinates":[[[${apiParameters.minimumlongitude}, ${apiParameters.maximumlatitude}],[${apiParameters.minimumlongitude}, ${apiParameters.minimumlatitude}],[${apiParameters.maximumlongitude}, ${apiParameters.minimumlatitude}],[${apiParameters.maximumlongitude}, ${apiParameters.maximumlatitude}],[${apiParameters.minimumlongitude}, ${apiParameters.maximumlatitude}]]]}}]}`);
+  const apiUrl = `${COMMONMOVES_API_BASE}/bin/bhhs/CregPropertySearchServlet?SearchType=Map&MinPrice=${apiParameters.minprice}&PropertyType=1,2,4&ApplicationType=FOR_SALE&ListingStatus=1&NewListing=${apiParameters.newlisting}&Sort=${apiParameters.sort}&PageSize=${apiParameters.pagesize}&Page=1&NorthEastLatitude=${apiParameters.maximumlatitude}&NorthEastLongitude=${apiParameters.maximumlongitude}&SouthWestLatitude=${apiParameters.minimumlatitude}&SouthWestLongitude=${apiParameters.minimumlongitude}&SearchParameter=${apiParameters.searchparameter}&MapSearchType=MapBounds&listingAgentId=&ucsid=false`;
+  const resp = await fetch(apiUrl);
   if (resp.ok) {
     const data = await resp.json();
     const listings = data.properties;
-    const cards = listings.slice(0, 8);
     const ul = document.createElement('ul');
-    cards.forEach((listing) => {
+    listings.forEach((listing) => {
       ul.innerHTML += createCard(listing);
     });
     decorateIcons(ul);
