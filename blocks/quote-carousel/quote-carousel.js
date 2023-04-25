44cba42868f18208e100121aab855124dd5647aa
Brendan Robert <brobert@adobe.com>
2023-04-25 15:06:19 -0500

    Revised to use separate k-v title pair

--- blocks/quote-carousel/quote-carousel.js
+++ blocks/quote-carousel/quote-carousel.js
@@ -106,10 +106,15 @@ async function getContent(block) {
   return data;
 }
 
+function getTitle(block) {
+  const titleElem = [...block.querySelectorAll("div")].filter(e=>e.innerText.toLowerCase()==="title");
+  return titleElem.length > 0 ? titleElem[0].nextElementSibling.innerText : "";
+}
+
 export default async function decorate(block) {
   const blockId = crypto.randomUUID();
-  const content = await getContent(block);
-  const title = block.querySelector('div > div > div > div > div').textContent;
+  const content = await getContent(block);  
+  const title = getTitle(block);
   // generate carousel content from loaded data
   const slidesContainer = document.createElement('div');
   slidesContainer.classList.add('carousel-content');
