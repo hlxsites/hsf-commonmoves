89fc267f8aa87fc5b4223c0fab4c3fded087bc41
Brendan Robert <brobert@adobe.com>
2023-05-10 15:57:56 -0500

    fix longitude label

--- blocks/liveby-map/liveby-map-delayed.js
+++ blocks/liveby-map/liveby-map-delayed.js
@@ -169,7 +169,7 @@ function initLiveByMap() {
     const map = new google.maps.Map(mapDiv, {
         zoom: 15,
         maxZoom: 18,
-        center: { lat: parseFloat(mapCenter[0]), lon: parseFloat(mapCenter[1]) },
+        center: { lat: parseFloat(mapCenter[0]), lng: parseFloat(mapCenter[1]) },
         mapTypeId: "roadmap",
         clickableIcons: false,
         gestureHandling: "greedy",
