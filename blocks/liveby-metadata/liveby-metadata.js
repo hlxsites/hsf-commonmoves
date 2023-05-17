1ec08bc49b63c36ad569d6a1268e5a3c4b2592ef
Brendan Robert <brobert@adobe.com>
2023-05-17 13:13:40 -0500

    ....

--- blocks/liveby-metadata/liveby-metadata.js
+++ blocks/liveby-metadata/liveby-metadata.js
@@ -17,7 +17,7 @@ async function getGeometry(id) {
   if (resp.ok) {
     const data = await resp.json();
     /* eslint-disable-next-line no-underscore-dangle */
-    const { geometry } = data.data.geometry;
+    const { geometry } = data.data;
     geometry.bbox = data.data.properties.bbox;
     geometry.centroid = data.data.properties.centroid;
     return geometry;
