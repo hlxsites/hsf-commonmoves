9ce4733fbc606b2588df7745cebe62d23fec2cb2
Bryan Stopp <bryan.stopp@gmail.com>
2023-05-19 14:27:49 -0500

    Async the JS.

--- blocks/juicer/juicer.js
+++ blocks/juicer/juicer.js
@@ -7,11 +7,13 @@ function loadEmbeds() {
   style.media = 'all';
   style.rel = 'stylesheet';
   style.type = 'text/css';
+
   document.head.append(style);
 
   const script = document.createElement('script');
   script.type = 'text/javascript';
   script.src = 'https://assets.juicer.io/embed.js';
+  script.async = true;
   document.head.append(script);
 }
 
