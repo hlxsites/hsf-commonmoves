4260a450a339b8b8774101c3fe42a90a15417523
Brendan Robert <brobert@adobe.com>
2023-05-22 14:45:22 -0500

    take two...

--- blocks/property-listing/property-listing.js
+++ blocks/property-listing/property-listing.js
@@ -78,10 +78,10 @@ export default async function decorate(block) {
     search = new RadiusSearch(lat, lon, radius);
   } else if (type === SearchType.Community) {
     const [bbox] = window.liveby.geometry;
-    const minLon = Math.min(...bbox.map((e => e[0])));
-    const maxLon = Math.max(...bbox.map((e => e[0])));
-    const minLat = Math.min(...bbox.map((e => e[1])));
-    const maxLat = Math.max(...bbox.map((e => e[1])));
+    const minLon = Math.min(...bbox.map((e) => e[0]));
+    const maxLon = Math.max(...bbox.map((e) => e[0]));
+    const minLat = Math.min(...bbox.map((e) => e[1]));
+    const maxLat = Math.max(...bbox.map((e) => e[1]));
     search = new MapSearch(minLat, minLon, maxLat, maxLon);
   }
 
