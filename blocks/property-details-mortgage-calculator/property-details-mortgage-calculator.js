9fe802d35c64286adc0acf2a74e38e5690b163cf
Amy Zhao <azhao@adobe.net>
2023-05-25 17:38:15 -0500

    clean up CSS

--- blocks/property-details-mortgage-calculator/property-details-mortgage-calculator.js
+++ blocks/property-details-mortgage-calculator/property-details-mortgage-calculator.js
@@ -1,15 +1,10 @@
 import { createAccordionItem } from '../../scripts/accordion.js';
 import { loadCSS } from '../../scripts/lib-franklin.js';
 import { computeMortgage, addCommaSeparators, nFormatter } from './compute-mortgage.js';
-const propertyAPI = 'https://www.commonmoves.com/bin/bhhs/CregPropertySearchServlet?ucsid=false&SearchType=Radius&ApplicationType=FOR_SALE&Sort=PRICE_ASCENDING&PageSize=9&MinPrice=7497500&MaxPrice=22492500&Latitude=42.56574249267578&Longitude=-70.76632690429688&Distance=2&CoverageZipcode=&teamNearBy=&teamCode=';
-const propID = '343140756';
 
 export default async function decorate(block) {
-  const resp = await fetch(propertyAPI);
-  if (resp.ok) {
-    const data = await resp.json();
-    const listing = data.properties.find((item) => item.PropId == propID);
-    const listPriceUSD = listing.ListPriceUS;
+  if(window.property) {
+    const listPriceUSD = window.property.ListPriceUS;
     const listPrice = listPriceUSD.replace(/[^\d.]+/g, '');
     const maxPurchasePrice = 2 * listPrice + 1000;
     const initialMortgageCalc = computeMortgage(listPrice);
--- blocks/property-details-schools/property-details-schools.js
+++ blocks/property-details-schools/property-details-schools.js
@@ -1,7 +1,21 @@
 import { createAccordionItem } from '../../scripts/accordion.js';
 import { decorateIcons, loadCSS } from '../../scripts/lib-franklin.js';
 
-const schoolAPI = 'https://www.commonmoves.com/bin/bhhs/cregSchoolServlet?latitude=42.56574249267578&longitude=-70.76632690429688';
+const urlParams = new URLSearchParams(window.location.search);
+export const DOMAIN = urlParams.get('env') === 'stage' ? 'ignite-staging.bhhs.com' : 'www.bhhs.com';
+const API_URL = `https://${DOMAIN}/bin/bhhs`;
+
+async function getSchools(latitude, longitude) {
+  const resp = await fetch(`${API_URL}/cregSchoolServlet?latitude=${latitude}&longitude=${longitude}`);
+  if (resp.ok) {
+    const data = await resp.json();
+    /* eslint-disable-next-line no-underscore-dangle */
+    return data;
+  }
+  /* eslint-disable-next-line no-console */
+  console.log('Unable to retrieve schools.');
+  return undefined;
+}
 
 function createSchoolTableHTML(type, schools) {
   var schoolHTML = `
@@ -49,140 +63,141 @@ function createSchoolTableHTML(type, schools) {
 }
 
 export default async function decorate(block) {
-  const resp = await fetch(schoolAPI);
-  if (resp.ok) {
-    const schoolData = await resp.json();
-    var publicSchools = schoolData.schoolResults.public;
-    var privateSchools = schoolData.schoolResults.private;
-    var schoolsHTML = `
-      <div class="schools-wrap">
-        <div id="school-table" class="schools-table">
-    `;
-    if (publicSchools.length != 0) {
-      schoolsHTML += createSchoolTableHTML('Public', publicSchools);
-    }
-    if (privateSchools.length != 0) {
-      schoolsHTML += createSchoolTableHTML('Private', privateSchools);
-    }
-    schoolsHTML += `
-        </div>
-        <div id="school-detail" class="cmp-local-school-detail d-none">
-          <div class="property-container">
-            <div class="property-row">
-              <div class="col col-12 col-md-10 offset-md-1">
-                <h5 class="cmp-local-school-detail__title"></h5>
-                <div class="cmp-local-school-detail__profile">
-                  <div class="property-row">
-                    <div class="col col-12 col-xl-6">
-                      <div class="property-row">
-                        <div class="col col-4">
-                          <h6>Grades</h6>
-                          <p class="cmp-local-school-detail__grade-range"></p>
-                        </div>
-                        <div class="col col-4">
-                          <h6>Students</h6>
-                          <p class="cmp-local-school-detail__num-students"></p>
-                        </div>
-                        <div class="col col-4">
-                          <h6>Type</h6>
-                          <p class="cmp-local-school-detail__school-type"></p>
-                        </div>
-                      </div>
-                      <h6>District</h6>
-                      <p class="cmp-local-school-detail__district-name"></p>
-                      <h6>Contact</h6>
-                      <p class="adr"></p>
-                      <p class="tel"></p>
-                      <p class="url">
-                        <a target="_blank" href=""></a>
-                      </p>
-                    </div>
-                    <div class="col col-12 col-xl-6 cmp-local-school-detail__boundary has-boundary">
-                      <div class="cmp-local-school-detail__map">
-                        <div id="school_gmap_canvas" style="position: relative; overflow: hidden;"></div>
-                        <div class="custom-controls">
-                          <div class="zoom-controls">
-                            <a class="map-zoom-in"></a> 
-                            <a class="map-zoom-out"></a>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="cmp-local-school-detail__more">
-                        <a href="#">
-                          <span class="icon icon-search"></span>
-                          See homes this school serves
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div>
-                  <div class="cmp-local-school-detail__stats">
+  if(window.property) {
+    const schools = await getSchools(window.property.Latitude, window.property.Longitude);
+    if(schools) {
+      var publicSchools = schools.schoolResults.public || [];
+      var privateSchools = schools.schoolResults.private || [];
+      var schoolsHTML = `
+        <div class="schools-wrap">
+          <div id="school-table" class="schools-table">
+      `;
+      if (publicSchools.length != 0) {
+        schoolsHTML += createSchoolTableHTML('Public', publicSchools);
+      }
+      if (privateSchools.length != 0) {
+        schoolsHTML += createSchoolTableHTML('Private', privateSchools);
+      }
+      schoolsHTML += `
+          </div>
+          <div id="school-detail" class="cmp-local-school-detail d-none">
+            <div class="property-container">
+              <div class="property-row">
+                <div class="col col-12 col-md-10 offset-md-1">
+                  <h5 class="cmp-local-school-detail__title"></h5>
+                  <div class="cmp-local-school-detail__profile">
                     <div class="property-row">
-                      <div class="col col-6">
-                        <h6>
-                          Students per Teacher
-                        </h6>
-                        <div class="cmp-local-school-detail__stat">
-                          <div class="val">
+                      <div class="col col-12 col-xl-6">
+                        <div class="property-row">
+                          <div class="col col-4">
+                            <h6>Grades</h6>
+                            <p class="cmp-local-school-detail__grade-range"></p>
                           </div>
-                          <!---->
+                          <div class="col col-4">
+                            <h6>Students</h6>
+                            <p class="cmp-local-school-detail__num-students"></p>
+                          </div>
+                          <div class="col col-4">
+                            <h6>Type</h6>
+                            <p class="cmp-local-school-detail__school-type"></p>
+                          </div>
+                        </div>
+                        <h6>District</h6>
+                        <p class="cmp-local-school-detail__district-name"></p>
+                        <h6>Contact</h6>
+                        <p class="adr"></p>
+                        <p class="tel"></p>
+                        <p class="url">
+                          <a target="_blank" href=""></a>
+                        </p>
+                      </div>
+                      <div class="col col-12 col-xl-6 cmp-local-school-detail__boundary has-boundary">
+                        <div class="cmp-local-school-detail__map">
+                          <div id="school_gmap_canvas" style="position: relative; overflow: hidden;"></div>
+                          <div class="custom-controls">
+                            <div class="zoom-controls">
+                              <a class="map-zoom-in"></a> 
+                              <a class="map-zoom-out"></a>
+                            </div>
+                          </div>
+                        </div>
+                        <div class="cmp-local-school-detail__more">
+                          <a href="#">
+                            <span class="icon icon-search"></span>
+                            See homes this school serves
+                          </a>
                         </div>
                       </div>
-                      <!---->
                     </div>
                   </div>
-                  <!---->
-                  <div class="cmp-local-school-detail__ratings">
-                    <h6>School Ratings</h6>
-                    <p>For school ratings and more information, please visit SchoolDigger.</p> 
-                    <a target="_blank" class="btn btn-secondary--compact">
-                      <span class="cmp-cta__btn-text">See Ratings</span>
-                    </a>
-                  </div>
-                  <div class="close" onclick="getElementById('school-table').classList.toggle('d-none');getElementById('school-detail').classList.toggle('d-none');">
-                    <span class="icon icon-close_x">
-                      <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="close-x" role="img" aria-hidden="true" tabindex="-1">
-                        <use xlink:href="/icons/icons.svg#close-x" role="presentation" aria-hidden="true" tabindex="-1"></use>
-                      </svg>
-                    </span>
+                  <div>
+                    <div class="cmp-local-school-detail__stats">
+                      <div class="property-row">
+                        <div class="col col-6">
+                          <h6>
+                            Students per Teacher
+                          </h6>
+                          <div class="cmp-local-school-detail__stat">
+                            <div class="val">
+                            </div>
+                            <!---->
+                          </div>
+                        </div>
+                        <!---->
+                      </div>
+                    </div>
+                    <!---->
+                    <div class="cmp-local-school-detail__ratings">
+                      <h6>School Ratings</h6>
+                      <p>For school ratings and more information, please visit SchoolDigger.</p> 
+                      <a target="_blank" class="btn btn-secondary--compact">
+                        <span class="cmp-cta__btn-text">See Ratings</span>
+                      </a>
+                    </div>
+                    <div class="close" onclick="getElementById('school-table').classList.toggle('d-none');getElementById('school-detail').classList.toggle('d-none');">
+                      <span class="icon icon-close_x">
+                        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="close-x" role="img" aria-hidden="true" tabindex="-1">
+                          <use xlink:href="/icons/icons.svg#close-x" role="presentation" aria-hidden="true" tabindex="-1"></use>
+                        </svg>
+                      </span>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    `;
-    var schoolAccordionItem = createAccordionItem('schools', 'Your Schools', schoolsHTML, schoolData.citation);
-    block.append(schoolAccordionItem);
-    decorateIcons(block);
-    loadCSS(`${window.hlx.codeBasePath}/styles/accordion.css`);
-    loadCSS(`${window.hlx.codeBasePath}/styles/property-details.css`);
-    
-    
-    var schoolNames = block.querySelectorAll('.school-name');
-    schoolNames.forEach((school) => {
-      school.addEventListener('click', (e) => {
-        var schoolTable = document.getElementById('school-table');
-        schoolTable.classList.toggle('d-none');
-        var schoolDetail = document.getElementById('school-detail');
-        schoolDetail.classList.toggle('d-none');
-        var target = e.currentTarget;
-        var schoolName = target.textContent.trim();
-        var school = [...privateSchools, ...publicSchools].find((s) => s.schoolName == schoolName);
-        schoolDetail.querySelector('.cmp-local-school-detail__title').textContent = school.schoolName;
-        schoolDetail.querySelector('.cmp-local-school-detail__grade-range').textContent = `${school.lowGrade}-${school.highGrade}`;
-        schoolDetail.querySelector('.cmp-local-school-detail__num-students').textContent = school.schoolYearlyDetails[0].numberOfStudents;
-        schoolDetail.querySelector('.cmp-local-school-detail__school-type').textContent = school.schoolLevel;
-        schoolDetail.querySelector('.cmp-local-school-detail__district-name').textContent = school.district.districtName ? school.district.districtName : "n/a";
-        schoolDetail.querySelector('.adr').textContent = school.addressFull;
-        schoolDetail.querySelector('.tel').textContent = school.phone
-        schoolDetail.querySelector('.url > a').textContent = school.url;
-        schoolDetail.querySelector('.url > a').setAttribute('href', school.url);
-        schoolDetail.querySelector('.cmp-local-school-detail__stat .val').textContent = school.schoolYearlyDetails[0].pupilTeacherRatio;
-        schoolDetail.querySelector('.cmp-local-school-detail__ratings a').setAttribute('href', school.urlCompare);
+      `;
+      var schoolAccordionItem = createAccordionItem('schools', 'Your Schools', schoolsHTML, schools.citation);
+      block.append(schoolAccordionItem);
+      decorateIcons(block);
+      loadCSS(`${window.hlx.codeBasePath}/styles/accordion.css`);
+      loadCSS(`${window.hlx.codeBasePath}/styles/property-details.css`);
+      
+      
+      var schoolNames = block.querySelectorAll('.school-name');
+      schoolNames.forEach((school) => {
+        school.addEventListener('click', (e) => {
+          var schoolTable = document.getElementById('school-table');
+          schoolTable.classList.toggle('d-none');
+          var schoolDetail = document.getElementById('school-detail');
+          schoolDetail.classList.toggle('d-none');
+          var target = e.currentTarget;
+          var schoolName = target.textContent.trim();
+          var school = [...privateSchools, ...publicSchools].find((s) => s.schoolName == schoolName);
+          schoolDetail.querySelector('.cmp-local-school-detail__title').textContent = school.schoolName;
+          schoolDetail.querySelector('.cmp-local-school-detail__grade-range').textContent = `${school.lowGrade}-${school.highGrade}`;
+          schoolDetail.querySelector('.cmp-local-school-detail__num-students').textContent = school.schoolYearlyDetails[0].numberOfStudents;
+          schoolDetail.querySelector('.cmp-local-school-detail__school-type').textContent = school.schoolLevel;
+          schoolDetail.querySelector('.cmp-local-school-detail__district-name').textContent = school.district.districtName ? school.district.districtName : "n/a";
+          schoolDetail.querySelector('.adr').textContent = school.addressFull;
+          schoolDetail.querySelector('.tel').textContent = school.phone
+          schoolDetail.querySelector('.url > a').textContent = school.url;
+          schoolDetail.querySelector('.url > a').setAttribute('href', school.url);
+          schoolDetail.querySelector('.cmp-local-school-detail__stat .val').textContent = school.schoolYearlyDetails[0].pupilTeacherRatio;
+          schoolDetail.querySelector('.cmp-local-school-detail__ratings a').setAttribute('href', school.urlCompare);
+        });
       });
-    });
+    }
   }
 }
