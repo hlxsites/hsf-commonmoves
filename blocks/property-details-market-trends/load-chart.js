00858471db309c58037875d73bf134fab24233fb
Amy Zhao <azhao@adobe.net>
2023-05-25 18:09:55 -0500

    fix market trends linting

--- blocks/property-details-market-trends/load-chart.js
+++ blocks/property-details-market-trends/load-chart.js
@@ -1,67 +1,63 @@
-import { nFormatter } from "../property-details-mortgage-calculator/compute-mortgage.js";
+import { nFormatter } from '../property-details-mortgage-calculator/compute-mortgage.js';
 
-Chart.Tooltip.positioners.custom = function(items) {
+// eslint-disable-next-line no-undef, func-names
+Chart.Tooltip.positioners.custom = function (items) {
+  // eslint-disable-next-line no-undef
   const pos = Chart.Tooltip.positioners.average(items);
-
   // Happens when nothing is found
   if (pos === false) {
     return false;
   }
-
-  const chart = this.chart;
-  
   return {
     x: pos.x,
     y: pos.y,
     xAlign: 'center',
-    yAlign: pos.y < chart.chartArea.bottom - 80 ? 'top' : 'bottom',
+    yAlign: pos.y < this.chart.chartArea.bottom - 80 ? 'top' : 'bottom',
   };
 };
 
-const labelFunc = (tooltipItem) => {
-  return '$' + tooltipItem.formattedValue;
-}
+const labelFunc = (tooltipItem) => `$${tooltipItem.formattedValue}`;
 
 const miniConfig = {
   type: 'line',
   options: {
     layout: {
-      padding: 10
+      padding: 10,
     },
     plugins: {
       legend: {
-        display: false
+        display: false,
       },
       tooltip: {
-        enabled: false
-      }
+        enabled: false,
+      },
     },
     scales: {
       x: {
         border: {
-          display: false
-        },
-        ticks: {
-          display: false
-        },
-        grid: {
-          display: false
-        }
-      },
-      y: {
-        border: {
-          display: false
+          display: false,
         },
         ticks: {
           display: false,
-          suggestedMin: 1E5
         },
         grid: {
-          display: false
-        }
-      }
-    }
-  }
+          display: false,
+        },
+      },
+      y: {
+        border: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+          suggestedMin: 1E5,
+        },
+        grid: {
+          display: false,
+        },
+      },
+    },
+  },
 };
 
 const detailConfig = {
@@ -69,7 +65,7 @@ const detailConfig = {
   options: {
     plugins: {
       legend: {
-        display: false
+        display: false,
       },
       tooltip: {
         enabled: true,
@@ -78,7 +74,7 @@ const detailConfig = {
         bodyFont: {
           size: 22,
           family: 'ProximaNova',
-          weight: 300
+          weight: 300,
         },
         bodyAlign: 'center',
         caretPadding: 12,
@@ -90,165 +86,160 @@ const detailConfig = {
         titleAlign: 'center',
         titleFont: {
           family: 'ProximaNova',
-          weight: 300
+          weight: 300,
         },
         padding: {
           x: 15,
-          y: 10
+          y: 10,
         },
         callbacks: {
-          title: (tooltipItems) => {
-            return tooltipItems[0].label.toUpperCase();
-          }
-        }
-      }
+          title: (tooltipItems) => tooltipItems[0].label.toUpperCase(),
+        },
+      },
     },
     hover: {
       mode: 'index',
-      intersect: false
+      intersect: false,
     },
     scales: {
       x: {
         border: {
-          display: false
+          display: false,
         },
         ticks: {
           autoSkip: false,
           maxRotation: 0,
           padding: 10,
-          callback: function(value, index, ticks)           {
-             return ticks.length - 1 == index || index == 0 ? this.getLabelForValue(value).toUpperCase() : ""
-          }
+          callback: (value, index, ticks) => ticks.length - 1 === index || (index === 0 ? this.getLabelForValue(value).toUpperCase() : ''),
         },
         grid: {
-          drawTicks: false
-        }
+          drawTicks: false,
+        },
       },
       y: {
         border: {
-          display: false
+          display: false,
         },
         ticks: {
           maxRotation: 0,
           padding: 10,
           suggestedMin: 5E4,
-          callback: function(value, index, ticks)           {
-             return ticks.length - 1 == index ? nFormatter(value, 0) : ""
-          }
+          callback: (value, index, ticks) => (ticks.length - 1 === index ? nFormatter(value, 0) : ''),
         },
         grid: {
           display: false,
-          drawTicks: false
-        }
-      }
-    }
-  }
+          drawTicks: false,
+        },
+      },
+    },
+  },
 };
 
 function initChart(ctx, xValues, yValues, dir, beginAtZero = false, mini = true) {
-  const backgroundColor = dir == "down" ? 'rgba(131, 43, 57, 0.1)' : 'rgba(43, 131, 79, 0.1)';
-  var borderColor = dir == "down" ? '#832B39' : '#2B834F';
-  var radius = new Array(xValues.length).fill(0);
-  if(mini) {
-    radius.splice(-1,1,3);
+  const backgroundColor = dir === 'down' ? 'rgba(131, 43, 57, 0.1)' : 'rgba(43, 131, 79, 0.1)';
+  const borderColor = dir === 'down' ? '#832B39' : '#2B834F';
+  const radius = new Array(xValues.length).fill(0);
+  if (mini) {
+    radius.splice(-1, 1, 3);
     miniConfig.options.scales.y.beginAtZero = beginAtZero;
     miniConfig.data = {
       labels: xValues,
       datasets: [{
         data: yValues,
         fill: true,
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
+        backgroundColor,
+        borderColor,
         borderWidth: 2,
         pointRadius: radius,
-        pointBackgroundColor: "#fff",
-        tension: 0.4
-      }]
+        pointBackgroundColor: '#fff',
+        tension: 0.4,
+      }],
     };
+    // eslint-disable-next-line no-undef
     return new Chart(ctx, miniConfig);
-  } else {
-    radius.splice(-1,1,4);
-    detailConfig.options.scales.y.beginAtZero = beginAtZero;
-    detailConfig.data = {
-      labels: xValues,
-      datasets: [{
-        data: yValues,
-        fill: true,
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
-        borderWidth: 3,
-        pointRadius: radius,
-        pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverBackgroundColor: '#3A3A3A',
-        pointHoverRadius: 5,
-        tension: 0.4
-      }]
-    };
-    return new Chart(ctx, detailConfig);
   }
+  radius.splice(-1, 1, 4);
+  detailConfig.options.scales.y.beginAtZero = beginAtZero;
+  detailConfig.data = {
+    labels: xValues,
+    datasets: [{
+      data: yValues,
+      fill: true,
+      backgroundColor,
+      borderColor,
+      borderWidth: 3,
+      pointRadius: radius,
+      pointBackgroundColor: '#fff',
+      pointBorderWidth: 1,
+      pointHoverBackgroundColor: '#3A3A3A',
+      pointHoverRadius: 5,
+      tension: 0.4,
+    }],
+  };
+  // eslint-disable-next-line no-undef
+  return new Chart(ctx, detailConfig);
 }
 
 function getChange(values) {
-  var change = (values.slice(-1)[0] - values.slice(-2)[0]) / values.slice(-2)[0];
-  return change < 0 ? "down" : "up";
+  const change = (values.slice(-1)[0] - values.slice(-2)[0]) / values.slice(-2)[0];
+  return change < 0 ? 'down' : 'up';
 }
 
-if(window.marketTrends) {
-  var data = window.marketTrends;
-  var trends = data.detailTrends;
-  var months = trends.map((item) => new Date(item.startDate).toLocaleString('default', { month: "short" }));
-  months.splice(-1, 1, "Current");
-  var medianListPrice = trends.map((item) => Number(item.medianListPrice.replace(/[^0-9.-]+/g,"")));
-  var medianSoldPrice = trends.map((item) => Number(item.medianSalesPrice.replace(/[^0-9.-]+/g,"")));
-  var avgPrice = trends.map((item) => Number(item.avgPriceArea.replace(/[^0-9.-]+/g,"")));
-  var homesSold = trends.map((item) => item.homesSold);
-  var homesSale = trends.map((item) => item.homesForSale);
-  var avgDays = trends.map((item) => item.avgDaysOnMarket);
+if (window.marketTrends) {
+  const data = window.marketTrends;
+  const trends = data.detailTrends;
+  const months = trends.map((item) => new Date(item.startDate).toLocaleString('default', { month: 'short' }));
+  months.splice(-1, 1, 'Current');
+  const medianListPrice = trends.map((item) => Number(item.medianListPrice.replace(/[^0-9.-]+/g, '')));
+  const medianSoldPrice = trends.map((item) => Number(item.medianSalesPrice.replace(/[^0-9.-]+/g, '')));
+  const avgPrice = trends.map((item) => Number(item.avgPriceArea.replace(/[^0-9.-]+/g, '')));
+  const homesSold = trends.map((item) => item.homesSold);
+  const homesSale = trends.map((item) => item.homesForSale);
+  const avgDays = trends.map((item) => item.avgDaysOnMarket);
 
-  var medianListPriceLineChart = null;
-  var medianSoldPriceLineChart = null;
-  var avgPriceLineChart = null;
-  var homesSoldLineChart = null;
-  var homesSaleLineChart = null;
-  var avgDaysLineChart = null;
+  let medianListPriceLineChart = null;
+  let medianSoldPriceLineChart = null;
+  let avgPriceLineChart = null;
+  let homesSoldLineChart = null;
+  let homesSaleLineChart = null;
+  let avgDaysLineChart = null;
 
-  var div = document.getElementsByClassName('cmp-property-details-market-trends__wrap')[0];
+  const div = document.getElementsByClassName('cmp-property-details-market-trends__wrap')[0];
 
-  function initCharts() {
-    medianListPriceLineChart = medianListPriceLineChart || initChart(document.getElementById("medianlistprice-line-chart"), months, medianListPrice, getChange(medianListPrice), true);
-    medianSoldPriceLineChart = medianSoldPriceLineChart || initChart(document.getElementById("mediansoldprice-line-chart"), months, medianSoldPrice, getChange(medianSoldPrice), true);
-    avgPriceLineChart = avgPriceLineChart || initChart(document.getElementById("avgprice-line-chart"), months, avgPrice, getChange(avgPrice));
-    homesSoldLineChart = homesSoldLineChart || initChart(document.getElementById("homessold-line-chart"), months, homesSold, getChange(homesSold));
-    homesSaleLineChart = homesSaleLineChart || initChart(document.getElementById("homesforsale-line-chart"), months, homesSale, getChange(homesSale));
-    avgDaysLineChart = avgDaysLineChart || initChart(document.getElementById("avgdays-line-chart"), months, avgDays, getChange(avgDays)); 
-  }
+  const initCharts = () => {
+    medianListPriceLineChart = medianListPriceLineChart || initChart(document.getElementById('medianlistprice-line-chart'), months, medianListPrice, getChange(medianListPrice), true);
+    medianSoldPriceLineChart = medianSoldPriceLineChart || initChart(document.getElementById('mediansoldprice-line-chart'), months, medianSoldPrice, getChange(medianSoldPrice), true);
+    avgPriceLineChart = avgPriceLineChart || initChart(document.getElementById('avgprice-line-chart'), months, avgPrice, getChange(avgPrice));
+    homesSoldLineChart = homesSoldLineChart || initChart(document.getElementById('homessold-line-chart'), months, homesSold, getChange(homesSold));
+    homesSaleLineChart = homesSaleLineChart || initChart(document.getElementById('homesforsale-line-chart'), months, homesSale, getChange(homesSale));
+    avgDaysLineChart = avgDaysLineChart || initChart(document.getElementById('avgdays-line-chart'), months, avgDays, getChange(avgDays));
+  };
 
-  if(window.innerWidth >= 992) {
+  if (window.innerWidth >= 992) {
     initCharts();
   }
   window.addEventListener('resize', () => {
-    if(window.innerWidth >= 992) {
+    if (window.innerWidth >= 992) {
       initCharts();
     }
   });
 
-  var table = document.getElementById('cmp-property-details-market-trends__table');
-  var detail = document.getElementById('cmp-property-details-market-trends__detail');
-  var detailLineChart = initChart(document.getElementById('detail-line-chart'), months, new Array(months.length).fill(100), "up", false, false);
+  const table = document.getElementById('cmp-property-details-market-trends__table');
+  const detail = document.getElementById('cmp-property-details-market-trends__detail');
+  const detailLineChart = initChart(document.getElementById('detail-line-chart'), months, new Array(months.length).fill(100), 'up', false, false);
 
   div.querySelectorAll('.cmp-property-details-market-trends__table .chart').forEach((chart) => {
     chart.addEventListener('click', (e) => {
-      var elem = e.currentTarget;
-      var dataElem = elem.previousElementSibling;
-      var label = dataElem.firstElementChild.textContent;
-      var value = dataElem.lastElementChild.textContent;
-      var title = document.querySelector('.cmp-property-details-market-trends__detail__title');
+      const elem = e.currentTarget;
+      const dataElem = elem.previousElementSibling;
+      const label = dataElem.firstElementChild.textContent;
+      const value = dataElem.lastElementChild.textContent;
+      const title = document.querySelector('.cmp-property-details-market-trends__detail__title');
       title.innerHTML = `${label} (1 Year)`;
-      var val = document.querySelector('.cmp-property-details-market-trends__detail__value');
+      const val = document.querySelector('.cmp-property-details-market-trends__detail__value');
       val.innerHTML = value;
-      var chartData = detailLineChart.data.datasets[0].data;
-      switch(e.currentTarget.id) {
+      let chartData = detailLineChart.data.datasets[0].data;
+      switch (e.currentTarget.id) {
         case 'medianlistprice':
           chartData = medianListPrice;
           detailLineChart.options.plugins.tooltip.callbacks.label = labelFunc;
@@ -272,21 +263,23 @@ if(window.marketTrends) {
         case 'homessold':
           chartData = homesSold;
           break;
-      };
-      const backgroundColor = getChange(chartData) == "down" ? 'rgba(131, 43, 57, 0.1)' : 'rgba(43, 131, 79, 0.1)';
-      var borderColor = getChange(chartData) == "down" ? '#832B39' : '#2B834F';
+        default:
+          break;
+      }
+      const backgroundColor = getChange(chartData) === 'down' ? 'rgba(131, 43, 57, 0.1)' : 'rgba(43, 131, 79, 0.1)';
+      const borderColor = getChange(chartData) === 'down' ? '#832B39' : '#2B834F';
       detailLineChart.data.datasets[0].data = chartData;
       detailLineChart.data.datasets[0].backgroundColor = backgroundColor;
       detailLineChart.data.datasets[0].borderColor = borderColor;
-      detailLineChart.update(); 
+      detailLineChart.update();
       table.classList.toggle('d-none');
       detail.classList.toggle('d-none');
     });
   });
 
-  var close = detail.querySelector('.close');
-    close.addEventListener('click', () => {
-      table.classList.toggle('d-none');
-      detail.classList.toggle('d-none');
+  const close = detail.querySelector('.close');
+  close.addEventListener('click', () => {
+    table.classList.toggle('d-none');
+    detail.classList.toggle('d-none');
   });
 }
