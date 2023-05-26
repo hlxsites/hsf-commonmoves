d667f05ac2c25e9a6880e3b5a45026eab1ddbbcf
Brendan Robert <brobert@adobe.com>
2023-05-26 10:39:17 -0500

    lint cleanup

--- blocks/community-directory/community-directory.js
+++ blocks/community-directory/community-directory.js
@@ -10,42 +10,41 @@ export async function getQueryIndex() {
 }
 
 export default async function decorate(block) {
-    const index = await getQueryIndex();
+  const index = await getQueryIndex();
 
-    const list = document.createElement('div');
-    list.classList.add('cards-list');
-    index.data.forEach((community) => {
-        const communityName = community["LiveBy Community"];
+  const list = document.createElement('div');
+  list.classList.add('cards-list');
+  index.data.forEach((community) => {
+    const communityName = community['LiveBy Community'];
 
-        const card = document.createElement('div');
-        list.append(card);
-        card.classList.add('cards-item');
+    const card = document.createElement('div');
+    card.classList.add('cards-item');
+    list.append(card);
 
-        const cardBody = document.createElement('div');
-        cardBody.classList.add('cards-item-body');
+    const cardBody = document.createElement('div');
+    cardBody.classList.add('cards-item-body');
+    card.append(cardBody);
 
-        card.append(cardBody);
-        cardBody.onclick = () => {
-            document.location.href = community.path;
+    cardBody.onclick = () => {
+      document.location.href = community.path;
+    };
+
+    // Add a listener to cardBody so that when it is active in the viewport,
+    // the background image is set
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          cardBody.style.backgroundImage = `url(${community.image})`;
+          observer.unobserve(cardBody);
         }
-
-        // Add a listener to cardBody so that when it is active in the viewport, the background image is set
-        const observer = new IntersectionObserver((entries) => {
-          entries.forEach((entry) => {
-            if (entry.isIntersecting) {
-              cardBody.style.backgroundImage = `url(${community.image})`;
-              observer.unobserve(cardBody);
-            }
-          });
-        });
-        observer.observe(cardBody);
-
-        cardBody.img = community.image;
-        cardBody.alt = communityName; 
-        const paragraphElement = document.createElement(('h4'));
-        paragraphElement.textContent = `Explore ${communityName}`;
-        cardBody.append(paragraphElement);
+      });
     });
-    block.classList.add(`cards`);
-    block.append(list);
+    observer.observe(cardBody);
+
+    const paragraphElement = document.createElement(('h4'));
+    paragraphElement.textContent = `Explore ${communityName}`;
+    cardBody.append(paragraphElement);
+  });
+  block.classList.add('cards');
+  block.append(list);
 }
