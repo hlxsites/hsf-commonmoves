31498e0842cb004fd929a261f69349d88b71303f
piyushjindal <piyushjindal@adobe.com>
2024-07-26 16:29:44 +0530

    Floating image design 1

--- blocks/floating-images/floating-images.js
+++ blocks/floating-images/floating-images.js
@@ -1,4 +1,10 @@
+import { getMetadata } from '../../scripts/aem.js';
+import { loadTemplateCSS } from '../../scripts/util.js';
+
 export default function decorate(block) {
+  const blockName = block.getAttribute('data-block-name');
+  const designType = getMetadata('template');
+  loadTemplateCSS(blockName, designType);
   const container = block.querySelector(':scope > div');
   container.children[0].classList.add('content');
   container.children[1].classList.add('image');
