85deadd327397ada68525277ea84c391dbf3fdda
piyushjindal <piyushjindal@adobe.com>
2024-07-27 22:04:19 +0530

    Quote Block, Hero Block bug fix

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,6 +1,10 @@
-import { readBlockConfig } from '../../scripts/aem.js';
+import { getMetadata, readBlockConfig } from '../../scripts/aem.js';
+import { loadTemplateCSS } from '../../scripts/util.js';
 
 export default async function decorate(block) {
+  const blockName = block.getAttribute('data-block-name');
+  const designType = getMetadata('template');
+  loadTemplateCSS(blockName, designType);
   const config = readBlockConfig(block);
 
   block.innerHTML = `
