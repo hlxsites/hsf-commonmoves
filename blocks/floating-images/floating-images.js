cb58fbc181ec5dda614b8d905058754b46c4a11b
Ritwik Srivastava <ritwik@ritwiks-mbp-2.corp.adobe.com>
2024-07-30 16:31:27 +0530

    Design3 changes

--- blocks/floating-images/floating-images.js
+++ blocks/floating-images/floating-images.js
@@ -1,5 +1,11 @@
-export default function decorate(block) {
+import { getMetadata, loadCSS } from '../../scripts/aem.js';
+
+export default async function decorate(block) {
   const container = block.querySelector(':scope > div');
   container.children[0].classList.add('content');
   container.children[1].classList.add('image');
+  const design = getMetadata('template');
+  const {blockName} = block.dataset;
+
+  await loadCSS(`${window.hlx.codeBasePath}/blocks/${blockName}/${design}.css`);
 }
