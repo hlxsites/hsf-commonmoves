91f6a2bb4b38a13c27ff15fc2eb5310e0254bc39
piyushjindal <piyushjindal@adobe.com>
2024-07-31 10:33:04 +0530

    Review Comments incorporated

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,10 +1,6 @@
-import { getMetadata, readBlockConfig } from '../../scripts/aem.js';
-import { loadTemplateCSS } from '../../scripts/util.js';
+import { readBlockConfig } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-  const blockName = block.getAttribute('data-block-name');
-  const designType = getMetadata('template');
-  loadTemplateCSS(blockName, designType);
   const config = readBlockConfig(block);
 
   block.innerHTML = `
